63e09442c506fa6a83cf6aefee89cf7039b9c7b9
yrkang <yrkang@nkia.co.kr>
2021-04-23 17:49:28 +0900

    content 스타일 수정, tabs 구현중

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,55 +1,46 @@
-
-import React, {useState} from 'react';
-
-const content = [
-    {
-        id: 1,
-        tab: 'Today',
-        action: () => today()
-    },
-    {
-        id: 2,
-        tab: 'This Week',
-        action: () => week()
-    },
-    {
-        id: 3,
-        tab: 'This Month',
-        action: () => month()
-    }
-]
-const today = () => {
-
-    console.log('today')
-}
-const week = () => {
-    console.log('week')
-}
-const month = () => {
-    console.log('month')
-}
-
-// const useTabs = (initialTab, allTabs) => {
-//     const [ activeTab , setActiveTab ] = useState(initialTab);
-//
-// }
+import React, { useState } from 'react'
 const Tabs = () => {
-    // const currentItem = useTabs(0, content);
-
-    return(
+    const [activeTab, setActiveTab] = useState(0)
+    const content = [
+        {
+            id: 1,
+            tab: 'Today',
+            action: () => today(),
+        },
+        {
+            id: 2,
+            tab: 'This Week',
+            action: () => week(),
+        },
+        {
+            id: 3,
+            tab: 'This Month',
+            action: () => month(),
+        },
+    ]
+    const today = () => {
+        console.log('today')
+    }
+    const week = () => {
+        console.log('week')
+    }
+    const month = () => {
+        console.log('month')
+    }
+    const isOn = id => {
+        setActiveTab(id)
+    }
+    return (
         <div className="tabs-wrapper">
             {content.map((section, i) => (
                 <div
-                    className={"tabs-wrapper-item"}
-                     key={section.id}
-                     onClick={section.action}
+                    className={'tabs-wrapper-item'}
+                    key={section.id}
+                    onClick={section.action}
                 >
                     {section.tab}
                 </div>
             ))}
-            <div>
-                {/*{currentItem.content}*/}
-            </div>
         </div>
     )
 }
